a059ac1ec9ecd85ac4e703238eca76d8e8885182
keros1n <chachimin@gmail.com>
2024-01-24 01:35:21 +0300

    first clean phyton file

---
+++ work_days-calculating.py
@@ -0,0 +1,11 @@
+# Код для расчёта рабочих дней по заданным датам начала и окончания периода
+import pandas as pd
+import numpy as np
+
+pd.options.display.colheader_justify = 'center'
+pd.options.display.max_columns = 200
+pd.options.display.max_colwidth = 100
+pd.options.display.float_format = '{:,.3f}'.format
+
+print ('Hello, buster!')
+print (f'Приятно когда {8/2} = 4')
